8a9e9ca518005f23b74c8544edb44e2d418b7a69
123Nira <77059367+123Nira@users.noreply.github.com>
2021-04-10 16:08:28 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,41 @@
-import React, {Component, useState} from "react";
+import React, {Component,createRef, useState} from "react";
 import '../styles/App.css';
 
 class App extends Component {
-    render() {
+    constructor(props){
+        super(props)
+        this.state ={
+            
+            time: new Date(),  
+        }
+        //this.timerid = setInterval(()=>this.foo,1000)
+       
+       // this.state.tm =  setInterval(()=>{  this.setState({time : new Date(), })},1000)
+        this.foo  = this.foo.bind(this);
+    }
 
+    foo(){
+        this.setState({time: new Date()})
+    }
+
+   
+    componentDidMount(){
+        this.timerid = setInterval(this.foo,1000)
+        //console.log("this runs");
+    }   
+    componentWillUnmount(){
+        clearInterval(this.timerid);
+    }
+  
+    
+    render() {
+       
         return(
-            <>
-               
-            </>
-        )
+            <div className = "Clock">
+                <h2 id="time">{this.state.time.toLocaleTimeString()}</h2>
+            </div>
+        );
     }
 }
-
 
 export default App;
